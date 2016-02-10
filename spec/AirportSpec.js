384a70e102dc8d55e20407d1d3c1b8c09c0eb7f9
Ajitsy <sanghera.p@gmail.com>
2016-02-10 19:49:51 +0000

    air traffic controller can instruct a plane to land and get confirmation of its landing

---
+++ spec/AirportSpec.js
@@ -0,0 +1,21 @@
+
+describe('Airport', function(){
+
+  var airport;
+  var plane;
+
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane');
+  });
+
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+  
+});
